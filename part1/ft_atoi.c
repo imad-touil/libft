f6e9bed51cdbe223c928ee82a66a7ef64432448e
imad-touil <imadtouil.it@gmail.com>
2024-10-14 22:41:27 +0100

    ft_atoi

---
+++ part1/ft_atoi.c
@@ -0,0 +1,24 @@
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	r;
+	int	s;
+
+	i = 0;
+	r = 0;
+	s = 1;
+	while (str[i] <= 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			s *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		r = r * 10 + str[i] - 48;
+		i++;
+	}
+	return (r * s);
+}
